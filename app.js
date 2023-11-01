20abec16fdee26a61671eec2ecd5c4da885ff9f7
Ranok Raihan <ranokraihan@gmail.com>
2023-11-02 00:32:59 +0600

    project finished

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const dateInput = document.getElementById("start-date");
+const targetTable = document.querySelector(".target-table");
+const weekDays = [
+  "রবিবার",
+  "সোমবার",
+  "মঙ্গলবার",
+  "বুধবার",
+  "বৃহস্পতিবার",
+  "শুক্রবার",
+  "শনিবার",
+];
 
 const dateChangeHandler = (e) => {
-  console.log(e.target.value);
+  const startDateObject = new Date(e.target.value);
+  const currentMonth = startDateObject.getMonth();
+
+  targetTable.innerHTML = `
+      <tr>
+        <th>তারিখ</th>
+        <th>বার</th>
+      </tr>
+  `;
+
+  while (startDateObject.getMonth() === currentMonth) {
+    targetTable.innerHTML += `
+    <tr>
+      <td>${startDateObject.toLocaleDateString("Bn-BD")}</td>
+      <td>${weekDays[startDateObject.getDay()]}</td>
+    </tr>
+ `;
+    startDateObject.setDate(startDateObject.getDate() + 3);
+  }
 };
 
 dateInput.addEventListener("change", dateChangeHandler);

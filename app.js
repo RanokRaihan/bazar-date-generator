d93a748e1d45510e2db8e6686970b454ed1509d4
Ranok Raihan <ranokraihan@gmail.com>
2025-10-01 20:51:05 -0400

    refactor dateChangeHandler to streamline table rendering logic

--- app.js
+++ app.js
@@ -10,32 +10,32 @@ const weekDays = [
   "শনিবার",
 ];
 
-const dateChangeHandler = (e) => {
-  const startDateInput = e.target.value; // "2025-10-16"
-  const [y, m, d] = startDateInput.split("-").map(Number);
-  const date = new Date(y, m - 1, d); // always local midnight of chosen day
-
-  const startDateObject = new Date(date); // copy it
-  console.log(e.target.value);
-  console.log(startDateObject);
+const renderTable = (startDate) => {
+  const [y, m, d] = startDate.split("-").map(Number);
+  const date = new Date(y, m - 1, d);
+  const startDateObject = new Date(date);
   const currentMonth = startDateObject.getMonth();
 
   targetTable.innerHTML = `
-      <tr>
-        <th>তারিখ</th>
-        <th>বার</th>
-      </tr>
+    <tr>
+      <th>তারিখ</th>
+      <th>বার</th>
+    </tr>
   `;
 
   while (startDateObject.getMonth() === currentMonth) {
     targetTable.innerHTML += `
-    <tr>
-      <td>${startDateObject.toLocaleDateString()}</td>
-      <td>${weekDays[startDateObject.getDay()]}</td>
-    </tr>
- `;
+      <tr>
+        <td>${startDateObject.toLocaleDateString()}</td>
+        <td>${weekDays[startDateObject.getDay()]}</td>
+      </tr>
+    `;
     startDateObject.setDate(startDateObject.getDate() + 3);
   }
+};
+
+const dateChangeHandler = (e) => {
+  renderTable(e.target.value);
 };
 
 dateInput.addEventListener("change", dateChangeHandler);
